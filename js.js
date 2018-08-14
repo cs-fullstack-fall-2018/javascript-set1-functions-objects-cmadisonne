29526f9a3d0086d18b1de77afbbeffee17bec042
Lotus <cmadisonne@gmail.com>
2018-08-13 22:59:54 -0500

    Attempted

---
+++ js.js
@@ -0,0 +1,20 @@
+var person = {
+    firstName: "Jimmy",
+    lastName: "Page",
+    age: "62",
+    Zip: "00821",
+    firstName: "Rick",
+    lastName: "Nielsen",
+    age: "57",
+    Zip: "61016",
+    firstName: "Joe",
+    lastName: "Walsh",
+    age: "58",
+    Zip: "90001",
+    firstName: "Lemmy",
+    lastName: "Kilmister",
+    age: "57",
+    Zip: "21120",
+};
+
+//i dont know if i even created the list right and i certainly dont know where to go from here but this is a comment
